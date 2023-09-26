0572f867d67f3437b84ab7b99dfd8cf9b10c53fb
Tom <91008122+TotoooDev@users.noreply.github.com>
2023-09-26 15:57:24 +0200

    Finally linux compatible!

--- Banana/src/Core/Log.h
+++ Banana/src/Core/Log.h
@@ -5,14 +5,14 @@
 // Simple log and assert macros
 
 #ifdef BANANA_DEBUG
-	#define BANANA_INFO(msg, ...)  spdlog::info(msg, __VA_ARGS__)
-	#define BANANA_WARN(msg, ...)  spdlog::warn(msg, __VA_ARGS__)
-	#define BANANA_ASSERT(x, msg, ...) if (!x) { BANANA_ABORT(msg, __VA_ARGS__); }
+	#define BANANA_INFO(msg, ...)  spdlog::info(msg, ##__VA_ARGS__)
+	#define BANANA_WARN(msg, ...)  spdlog::warn(msg, ##__VA_ARGS__)
+	#define BANANA_ASSERT(x, msg, ...) if (!x) { BANANA_ABORT(msg, ##__VA_ARGS__); }
 #else
 	#define BANANA_INFO(msg, ...)
 	#define BANANA_WARN(msg, ...)
 	#define BANANA_ASSERT(x, msg, ...)
 #endif
 
-#define BANANA_ERROR(msg, ...) spdlog::error(msg, __VA_ARGS__)
-#define BANANA_ABORT(msg, ...) BANANA_ERROR(msg, __VA_ARGS__); std::abort()
+#define BANANA_ERROR(msg, ...) spdlog::error(msg, ##__VA_ARGS__)
+#define BANANA_ABORT(msg, ...) BANANA_ERROR(msg, ##__VA_ARGS__); std::abort()
